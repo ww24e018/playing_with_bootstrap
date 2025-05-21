1cf63a808972dfc4a9461ace1f6797d3edb4a9bb
ww24e018 <ww24e018@technikum-wien.at>
2025-05-21 14:27:39 +0200

    layout weird, aber gut genug um zu testen

--- files/webcomp_v1/comp_kartei.js
+++ files/webcomp_v1/comp_kartei.js
@@ -5,7 +5,7 @@ template.innerHTML = `
           integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
           crossorigin="anonymous">
 <div class="card" style="width: 18rem;">
-  <img src="" class="card-img-top" alt="...">
+  <img src="" class="card-img-top" alt="alt-text-for-kartei-image">
   <div class="card-body">
     <h5 class="card-title"><slot name="kartei-name">NEED NAME</slot></h5>
     <p class="card-text"><slot name="kartei-beschreibung">NEED BESCHREIBUNG</slot></p>
@@ -18,18 +18,17 @@ template.innerHTML = `
 class KarteiComponent extends HTMLElement {
   constructor() {
     super();
-	//this.attachShadow({mode: 'open'});
-	//this.shadowRoot.appendChild(template.content.cloneNode(true));
-
-    const template = document.getElementById('template-for-kartei-component');
-    const templateContent = template.content;
-
-    this.attachShadow({mode:'open'}).appendChild(
-        templateContent.cloneNode(true)
-    );
+	this.attachShadow({mode: 'open'});
+	this.shadowRoot.appendChild(template.content.cloneNode(true));
 
   }
   // Element functionality written in here
+
+  connectedCallback() {
+      this.shadowRoot.querySelector('img.card-img-top')
+          .src = `https://picsum.photos/id/${Math.round(Math.random()*210,0)}/300/200`;
+  }
+
 }
 
 window.customElements.define('kartei-component', KarteiComponent);
