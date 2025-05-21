b0bead0022c790efe29440054ca159c50f4292a8
ww24e018 <ww24e018@technikum-wien.at>
2025-05-21 14:05:38 +0200

    sidequest: ist dom-block in beide richtungen?; unklar. aber does not work

--- files/webcomp_v1/comp_kartei.js
+++ files/webcomp_v1/comp_kartei.js
@@ -1,7 +1,16 @@
 const template = document.createElement('template');
 template.innerHTML = `
-<div class="user-card">
-helloworld<slot name="ihrewerbung">Dumm<b>y</b>-Werbung</slot>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT"
+          crossorigin="anonymous">
+<div class="card" style="width: 18rem;">
+  <img src="" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><slot name="kartei-name">NEED NAME</slot></h5>
+    <p class="card-text"><slot name="kartei-beschreibung">NEED BESCHREIBUNG</slot></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
 </div>
 `
 
@@ -9,13 +18,18 @@ helloworld<slot name="ihrewerbung">Dumm<b>y</b>-Werbung</slot>
 class KarteiComponent extends HTMLElement {
   constructor() {
     super();
-	
-	this.attachShadow({mode: 'open'});
-	this.shadowRoot.appendChild(template.content.cloneNode(true));
-	this.shadowRoot.querySelector('')
+	//this.attachShadow({mode: 'open'});
+	//this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+    const template = document.getElementById('template-for-kartei-component');
+    const templateContent = template.content;
+
+    this.attachShadow({mode:'open'}).appendChild(
+        templateContent.cloneNode(true)
+    );
 
   }
   // Element functionality written in here
 }
 
-window.customElements.define("kartei-component", KarteiComponent);
+window.customElements.define('kartei-component', KarteiComponent);
