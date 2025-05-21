408796ed8b34ecc3454098e966a83eb8e2b73e2e
ww24e018 <ww24e018@technikum-wien.at>
2025-05-21 12:57:27 +0200

    testest

--- files/webcomp_v1/comp_kartei.js
+++ files/webcomp_v1/comp_kartei.js
@@ -1,6 +1,19 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<div class="user-card">
+helloworld<slot name="ihrewerbung">Dumm<b>y</b>-Werbung</slot>
+</div>
+`
+
+
 class KarteiComponent extends HTMLElement {
   constructor() {
     super();
+	
+	this.attachShadow({mode: 'open'});
+	this.shadowRoot.appendChild(template.content.cloneNode(true));
+	
+	
   }
   // Element functionality written in here
 }
