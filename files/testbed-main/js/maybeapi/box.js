ef61b1eb69487edbe24a0d96a3b34da03ce639c9
ww24e018 <ww24e018@technikum-wien.at>
2025-06-05 09:40:25 +0200

    make promise-return version, base *async on that one.

--- files/testbed-main/js/maybeapi/box.js
+++ files/testbed-main/js/maybeapi/box.js
@@ -2,24 +2,31 @@ import { backendUrls } from "./backendUrls.js";
 
 
 export var box = {
-    readAsync: function(id){
+    read: function(id) {
         return fetch(`${backendUrls.box}/${id}`)
             .then(
                 function(res){return res.json()},
                 function(err){ console.error(err); return err;}
             )
+    },
+    readAsync: function(id){
+        return this.read(id)
             .then(
                 function(json){ return json;},
                 function(err){console.error(err); return err;}
             )
 
     },
-    readAllAsync: function(){
+
+    readAll: function() {
         return fetch(`${backendUrls.box}`)
             .then(
                 function(res){return res.json()},
                 function(err){ console.error(err); return err;}
             )
+    },
+    readAllAsync: function(){
+        return this.readAll()
             .then(
                 function(json){ return json;},
                 function(err){console.error(err); return err;}
