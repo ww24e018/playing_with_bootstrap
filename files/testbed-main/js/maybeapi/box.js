58202ba6ff3bcba90f7452a101e0f1a2538bde68
ww24e018 <ww24e018@technikum-wien.at>
2025-06-05 09:51:16 +0200

    Update box.js

--- files/testbed-main/js/maybeapi/box.js
+++ files/testbed-main/js/maybeapi/box.js
@@ -1,21 +1,21 @@
 import { backendUrls } from "./backendUrls.js";
 
+// current thinking: it seems easier to read and debug if one only generalizes the error handlers
+let promiseHandler_request_success = function(res) { return res.json() } ;
+let promiseHandler_request_fail = function(err) { console.error(err); return err; };
+
+let promiseHandler_json_success = function(json) { return json } ;
+let promiseHandler_json_fail = function(err) { console.error(err); return err; };
+
 
 export var box = {
     read: function(id) {
         return fetch(`${backendUrls.box}/${id}`)
-            .then(
-                function(res){return res.json()},
-                function(err){ console.error(err); return err;}
-            )
+            .then((res) => res.json(), promiseHandler_request_fail)
     },
     readAsync: function(id){
         return this.read(id)
-            .then(
-                function(json){ return json;},
-                function(err){console.error(err); return err;}
-            )
-
+            .then((json) => json, promiseHandler_json_fail);
     },
 
     readAll: function() {
